3004e26e89b5c74caa6999ed8549a1000335ab3d
Nate Tate <nate91711@gmail.com>
2016-02-11 12:33:49 -0800

    makes the boards grid private

--- board.js
+++ board.js
@@ -1,21 +1,26 @@
+var _ = require('lodash');
+
+function initialize(height, width) {
+  return _(height).times(function () {
+    return _(width).times(function () {
+      return 0;
+    });
+  });
+}
+
 function Board (height, width) {
+  var grid = initialize(height, width);
   this.height = height;
   this.width = width;
-  this.grid = this.makeGrid(this.height, this.width);
+
+  this.get = function get(coords) {
+    return grid[coords.x][coords.y];
+  };
+
+  this.set = function set(coords, val) {
+    return grid[coords.x][coords.y] = val;
+  };
 };
 
-//TODO: Make function private
-Board.prototype.makeGrid = function makeGrid(height, width) {
-  return [[0,0,0],[0,0,0],[0,0,0]];
-}
-
-Board.prototype.set = function set(coords, val) {
-  return this.grid[coords.x][coords.y] = val;
-}
-
-Board.prototype.get = function get(coords) {
-  return this.grid[coords.x][coords.y];
-}
 
 module.exports = Board;
-
