781c5548e283b409c70363dbe4aac5f751519de7
Nate Tate <nate91711@gmail.com>
2016-02-10 17:03:08 -0800

    added getter/setter to board

--- board.js
+++ board.js
@@ -4,9 +4,18 @@ function Board (height, width) {
   this.grid = this.makeGrid(this.height, this.width);
 };
 
+//TODO: Make function private
 Board.prototype.makeGrid = function makeGrid(height, width) {
   return [[0,0,0],[0,0,0],[0,0,0]];
 }
 
-module.exports.Board;
+Board.prototype.set = function set(coords, val) {
+  return this.grid[coords.x][coords.y] = val;
+}
+
+Board.prototype.get = function get(coords) {
+  return this.grid[coords.x][coords.y];
+}
+
+module.exports = Board;
 
--- test/board_spec.js
+++ test/board_spec.js
@@ -1,10 +1,33 @@
 var assert = require('assert');
-var Game = require('../board');
+var Board = require('../board');
+
+function setup(options) {
+  var h = options.height || 3;
+  var w = options.width || 3;
+  return new Board(h, w);
+}
 
 describe('The Game Board', function () {
   it('should have a height and width property', function () {
-    var board = new Game.Board(3, 5);
+    var board = setup({height: 3, width: 5});
     assert.equal(board.height, 3);
     assert.equal(board.width, 5);
+  });
+
+  describe('getter/setter methods', function () {
+    it('should store an object at a location', function () {
+      var board = setup({height: 3, width: 5});
+      var orc = {name: 'Orc'};
+      var coord = {x:1, y:1};
+      board.set(coord, orc)
+      assert.equal(board.get(coord), orc);
+    });
+  });
+
+  describe('show', function () {
+    it('should print the state of the board', function () {
+      var board = setup({height: 3, width: 5});
+      assert.equal(board.show(), [[0,0,0],[0,0,0],[0,0,0]]);
+    });
   });
 });
